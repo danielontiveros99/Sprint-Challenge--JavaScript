ef3ff2b409765f22980e3294b96473dd4b271d46
Daniel <daniel-ontiveros@lambdastudents.com>
2020-08-14 20:23:24 -0700

    yay three stars!

--- challenges/closure.js
+++ challenges/closure.js
@@ -5,11 +5,11 @@
 
 const external = "I'm outside the function";
 
-function myFunction() {
+myFunction = () => {
   console.log(external);
   const internal = "Hello! I'm inside myFunction!";
 
-  function nestedFunction() {
+  nestedFunction = () => {
     console.log(internal);
   };
   nestedFunction();
@@ -21,4 +21,13 @@ myFunction();
 
 /* Task 2: Counter */
 
-/* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+/* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number.
+For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+summation = (num) => {
+  let sum = 0;
+  for (let i = 0; i <= num; i++){
+    sum += i;
+  }
+  return sum;
+}
+console.log(summation(4));
